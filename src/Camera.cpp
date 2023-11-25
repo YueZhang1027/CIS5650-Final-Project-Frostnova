8c4f070e8ebc7f5b3987a249e13de0f7e3098e7a
Yue Zhang <YueZhang1027@users.noreply.github.com>
2023-11-25 17:45:54 -0500

    Update camera

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -13,11 +13,16 @@ Camera::Camera(Device* device, float aspectRatio) : device(device) {
     theta = 0.0f;
     phi = 0.0f;
 
-    glm::vec3 eye = glm::vec3(0.0f, 325.0f, 10.0f);
+    glm::vec3 eye = glm::vec3(0.0f, 300.0f, 0.0f);
     cameraBufferObject.viewMatrix = glm::lookAt(eye, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     cameraBufferObject.projectionMatrix = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
     cameraBufferObject.projectionMatrix[1][1] *= -1; // y-coordinate is flipped
     cameraBufferObject.cameraPosition = glm::vec4(eye, 1.0f);
+
+    // phi, theta
+    theta = 0.0f;
+    phi = -90.0f;
+
 
     camBuffer.MapMemory(device, sizeof(CameraBufferObject));
     memcpy(camBuffer.mappedData, &cameraBufferObject, sizeof(CameraBufferObject));
@@ -61,10 +66,12 @@ void Camera::UpdateOrbit(float deltaX, float deltaY, float deltaZ) {
     float radPhi = glm::radians(phi);
 
     glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), radTheta, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::rotate(glm::mat4(1.0f), radPhi, glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 finalTransform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f)) * rotation * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.0f, r));
+    glm::mat4 finalTransform = glm::translate(glm::mat4(1.0f), glm::vec3(100.0f, 300.0f, 100.0f)) 
+        * rotation 
+        * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, r));
 
     cameraBufferObject.viewMatrix = glm::inverse(finalTransform);
-    cameraBufferObject.cameraPosition = glm::vec4(finalTransform[0][3], finalTransform[1][3], finalTransform[2][3], 1.0f);
+    cameraBufferObject.cameraPosition = finalTransform * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 
     memcpy(camBuffer.mappedData, &cameraBufferObject, sizeof(CameraBufferObject));
 }
--- src/shaders/tone.frag
+++ src/shaders/tone.frag
@@ -8,7 +8,27 @@ layout(location = 1) in vec2 fragTexCoord;
 
 layout(location = 0) out vec4 outColor;
 
+vec3 uc2Tonemap(vec3 x)
+{
+   return ((x*(0.15*x+0.1*0.5)+0.2*0.02)/(x*(0.15*x+0.5)+0.2*0.3))-0.02/0.3;
+}
+
+vec3 tonemap(vec3 x, float exposure, float invGamma, float whiteBalance) {
+    vec3 white = vec3(whiteBalance);
+    vec3 color = uc2Tonemap(exposure * x);
+    vec3 whitemap = 1.0 / uc2Tonemap(white);
+    color *= whitemap;
+    return pow(color, vec3(invGamma));
+}
+
 void main() {
-    outColor = texture(texColor, fragTexCoord);
-    //outColor = vec4(0.5 * (fragTexCoord + vec2(1.0)), 0.5 * (sin(3.14159 * 0.1) + 1.0), 1.0); //texture(texSampler, fragTexCoord);
+    vec3 col = texture(texColor, fragTexCoord).xyz;
+
+    // float whitepoint = 1.0;
+    // col = tonemap(col, 0.7, 1.0 / 2.2, whitepoint);
+    // 
+    // float vignette = dot(fragTexCoord - 0.5, fragTexCoord - 0.5);
+    // 
+    // col = mix(col, vec3(0.1, 0.05, 0.13), vignette);
+    outColor = vec4(col, 1.0);
 }
